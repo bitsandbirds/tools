38fff8cc6994a3d1d3ff2ac01e58c140ead5ebe7
daniel-bnb <daniel@bitsandbirds.de>
2020-08-29 18:08:47 +0200

    Update prepare-commit-msg.sh

--- prepare-commit-msg.sh
+++ prepare-commit-msg.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+##############################################
 # All credits go to this guy: https://gist.github.com/bartoszmajsak/1396344#file-prepare-commit-msg-sh
+##############################################
+
+
+# This way you can customize which branches should be skipped when
+# prepending commit message. 
+if [ -z "$BRANCHES_TO_SKIP" ]; then
+  BRANCHES_TO_SKIP=(master develop test)
+fi
+
 # This way, it will only work for branches that entail the JIRA ticket convention:  "ABC-123"
 
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD 2> /dev/null | grep -oE "[A-Z]+-[0-9]+")
