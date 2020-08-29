0e0c157f98d421901c235783466cff58b659f522
daniel-bnb <daniel@bitsandbirds.de>
2020-08-29 18:04:26 +0200

    Create prepare-commit-msg.sh

---
+++ prepare-commit-msg.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# All credits go to this guy: https://gist.github.com/bartoszmajsak/1396344#file-prepare-commit-msg-sh
+# This way, it will only work for branches that entail the JIRA ticket convention:  "ABC-123"
+
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD 2> /dev/null | grep -oE "[A-Z]+-[0-9]+")
+if [ -n "$BRANCH_NAME" ]; then
+    echo "[$BRANCH_NAME] $(cat $1)" > $1
+fi
+
+BRANCH_NAME=$(git symbolic-ref --short HEAD)
+BRANCH_NAME="${BRANCH_NAME##*/}"
+
+BRANCH_EXCLUDED=$(printf "%s\n" "${BRANCHES_TO_SKIP[@]}" | grep -c "^$BRANCH_NAME$")
+BRANCH_IN_COMMIT=$(grep -c "\[$BRANCH_NAME\]" $1)
+
+if [ -n "$BRANCH_NAME" ] && ! [[ $BRANCH_EXCLUDED -eq 1 ]] && ! [[ $BRANCH_IN_COMMIT -ge 1 ]]; then 
+  sed -i.bak -e "1s/^/[$BRANCH_NAME] /" $1
+fi
